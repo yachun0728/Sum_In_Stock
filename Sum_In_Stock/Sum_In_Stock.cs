7b0ed678adf6f9879c8e1b4fca0564ae20445526
yachun0728 <yachun0728@gmail.com>
2017-06-12 15:36:25 +0800

    Create new project

---
+++ Sum_In_Stock/Sum_In_Stock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sum_In_Stock
+{
+    public class Sum_In_Stock
+    {
+        public void GetData()
+        {
+        }
+
+    }
+
+}
---
+++ Sum_In_StockTests/Sum_In_StockTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sum_In_Stock;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sum_In_Stock.Tests
+{
+    [TestClass()]
+    public class Sum_In_StockTests
+    {
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
